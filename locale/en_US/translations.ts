b25c970c0975cbf176151bade121574abce31f00
Kyle Burk <kyleburk@live.com>
2023-03-19 15:12:28 -0500

    wip

---
+++ locale/en_US/translations.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US" sourcelanguage="en_US">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+  <name>default</name>
+  <message>
+    <source>Hello</source>
+    <translation>Bonjour</translation>
+  </message>
+  <message>
+    <source>Goodbye</source>
+    <translation>Au revoir</translation>
+  </message>
+  <message>
+    <source>Christmas</source>
+    <translation>Noel</translation>
+  </message>
+</context>
+</TS>
