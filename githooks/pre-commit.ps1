aee626c611b498689d7cebdbc1f6aa3ae9af2a7a
Kyle Burk <kyleburk@live.com>
2023-03-20 16:10:09 -0500

    Update git hooks

--- githooks/pre-commit.ps1
+++ githooks/pre-commit.ps1
@@ -3,3 +3,19 @@
 $version = gitversion | ConvertFrom-Json
 
 $manifest = Get-Content ./manifest
+$output = ""
+$manifest | ForEach-Object {
+    if ($_ -like "major_version=*") {
+        $output += $("major_version=$($version.major)`n")
+    }
+    elseif ($_ -like "minor_version=*") {
+        $output += $("minor_version=$($version.minor)`n")
+    }
+    elseif ($_ -like "build_version=*") {
+        $output += $("build_version=$("$($version.patch)".padleft(2,"0"))$($version.CommitsSinceVersionSource)`n")
+    }
+    else {
+        $output += "$($_)`n"
+    }
+}
+Set-Content ./manifest -Value $output
