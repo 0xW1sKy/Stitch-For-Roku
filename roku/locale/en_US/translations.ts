8da7f570e209c1e5227fb9622222474d3cf74c03
Kyle Burk <kyleburk@live.com>
2023-06-10 14:47:00 -0500

    Initial Localization

---
+++ roku/locale/en_US/translations.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US" sourcelanguage="en_US">
+    <defaultcodec>UTF-8</defaultcodec>
+    <context>
+        <name>default</name>
+        <message>
+            <source>Token0001</source>
+            <translation>Live Channels</translation>
+        </message>
+        <message>
+            <source>Token0002</source>
+            <translation>Clips</translation>
+        </message>
+        <message>
+            <source>Token0003</source>
+            <translation>Browse</translation>
+        </message>
+        <message>
+            <source>Token0004</source>
+            <translation>Categories</translation>
+        </message>
+        <message>
+            <source>Token0005</source>
+            <translation>Following</translation>
+        </message>
+        <message>
+            <source>Token0006</source>
+            <translation>Offline Channels</translation>
+        </message>
+        <message>
+            <source>Token0007</source>
+            <translation>Login</translation>
+        </message>
+                <message>
+            <source>Token0008</source>
+            <translation>Live Stream</translation>
+        </message>
+        <message>
+            <source>Token0009</source>
+            <translation>Recent Videos</translation>
+        </message>
+        <message>
+            <source>Token0010</source>
+            <translation>Search</translation>
+        </message>
+        <message>
+            <source>Token0011</source>
+            <translation>Go to https://www.twitch.tv/activate and type the following code:</translation>
+        </message>
+        <message>
+            <source>Token0012</source>
+            <translation>Stitch should log you in soon after you authorize your account for use.</translation>
+        </message>
+        <message>
+            <source>Token0013</source>
+            <translation>Options</translation>
+        </message>
+        <message>
+            <source>Token0014</source>
+            <translation>Video/Stream Quality</translation>
+        </message>
+        <message>
+            <source>Token0015</source>
+            <translation>Chat On-By-Default</translation>
+        </message>
+        <message>
+            <source>LIVE</source>
+            <translation>LIVE</translation>
+        </message>
+    </context>
+</TS>
