d936ca9f714e477945c3efff4ca3b44ae30187b4
Kyle Burk <kyleburk@live.com>
2023-06-10 17:05:32 -0500

    add es_419

---
+++ roku/locale/es_419/translations.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es_419" sourcelanguage="en_US">
+    <defaultcodec>UTF-8</defaultcodec>
+    <context>
+        <name>default</name>
+        <message>
+            <source>Live Channels</source>
+            <translation>Canales en vivo</translation>
+        </message>
+        <message>
+            <source>Clips</source>
+            <translation>Clips</translation>
+        </message>
+        <message>
+            <source>Browse</source>
+            <translation>Buscar</translation>
+        </message>
+        <message>
+            <source>Categories</source>
+            <translation>Categorías</translation>
+        </message>
+        <message>
+            <source>Following</source>
+            <translation>Siguiendo</translation>
+        </message>
+        <message>
+            <source>Offline Channels</source>
+            <translation>Canales sin conexión</translation>
+        </message>
+        <message>
+            <source>Login</source>
+            <translation>Iniciar sesión</translation>
+        </message>
+        <message>
+            <source>Live Stream</source>
+            <translation>Transmisión en vivo</translation>
+        </message>
+        <message>
+            <source>Recent Videos</source>
+            <translation>Vídeos recientes</translation>
+        </message>
+        <message>
+            <source>Search</source>
+            <translation>Buscar</translation>
+        </message>
+        <message>
+            <source>Go to https://www.twitch.tv/activate and type the following code:</source>
+            <translation>Ve a https://www.twitch.tv/activate y escribe el siguiente código:</translation>
+        </message>
+        <message>
+            <source>Stitch should log you in soon after you authorize your account for use.</source>
+            <translation>Stitch debería iniciar sesión poco después de autorizar tu cuenta para usarse.</translation>
+        </message>
+        <message>
+            <source>LIVE</source>
+            <translation>EN VIVO</translation>
+        </message>
+        <message>
+            <source>Chat On-By-Default</source>
+            <translation>Chat activado por defecto</translation>
+        </message>
+        <message>
+            <source>Determines default state of chat window when opening a stream</source>
+            <translation>Determina el estado por defecto de la ventana del chat al abrir una transmisión</translation>
+        </message>
+        <message>
+            <source>Video Quality</source>
+            <translation>Calidad del vídeo</translation>
+        </message>
+        <message>
+            <source>Video Framerate</source>
+            <translation>Velocidad de fotogramas del vídeo</translation>
+        </message>
+    </context>
+</TS>
