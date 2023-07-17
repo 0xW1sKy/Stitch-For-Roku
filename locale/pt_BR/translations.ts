ee2e91c1d66611a13eed210b40d92c180fd35a70
0xw1sky <kyleburk@live.com>
2023-07-17 18:56:59 -0500

    update translations

---
+++ locale/pt_BR/translations.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_BR" sourcelanguage="en_US">
+    <defaultcodec>UTF-8</defaultcodec>
+    <context>
+        <name>default</name>
+        <message>
+            <source>Live Channels</source>
+            <translation>Canais Ao Vivo</translation>
+        </message>
+        <message>
+            <source>Clips</source>
+            <translation>Cortes</translation>
+        </message>
+        <message>
+            <source>Browse</source>
+            <translation>Navegar</translation>
+        </message>
+        <message>
+            <source>Categories</source>
+            <translation>Categorias</translation>
+        </message>
+        <message>
+            <source>Following</source>
+            <translation>Seguindo</translation>
+        </message>
+        <message>
+            <source>Offline Channels</source>
+            <translation>Canais Offline</translation>
+        </message>
+        <message>
+            <source>Login</source>
+            <translation>Entrar</translation>
+        </message>
+        <message>
+            <source>Live Stream</source>
+            <translation>Transmissões Ao Vivo</translation>
+        </message>
+        <message>
+            <source>Recent Videos</source>
+            <translation>Vídeos recentes</translation>
+        </message>
+        <message>
+            <source>Search</source>
+            <translation>Pesquisar</translation>
+        </message>
+        <message>
+            <source>Go to https://www.twitch.tv/activate and type the following code:</source>
+            <translation>Abra https://www.twitch.tv/activate no navegador e digite o código abaixo:</translation>
+        </message>
+        <message>
+            <source>Stitch should log you in soon after you authorize your account for use.</source>
+            <translation>Stitch deve te conectar logo após você autorizar o uso da sua conta.</translation>
+        </message>
+        <message>
+            <source>LIVE</source>
+            <translation>AO VIVO</translation>
+        </message>
+        <message>
+            <source>Chat On-By-Default</source>
+            <translation>Chat ligado por padrão</translation>
+        </message>
+        <message>
+            <source>Determines default state of chat window when opening a stream</source>
+            <translation>Determina o padrão da janela de chat ao abrir uma transmissão</translation>
+        </message>
+        <message>
+            <source>Video Quality</source>
+            <translation>Qualidade de vídeo</translation>
+        </message>
+        <message>
+            <source>Video Framerate</source>
+            <translation>Taxa de atualização</translation>
+        </message>
+    </context>
+</TS>
