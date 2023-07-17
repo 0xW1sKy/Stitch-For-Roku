14bf637192fde59128522e5bbbe2dbf6037a105b
0xw1sky <kyleburk@live.com>
2023-07-17 18:56:56 -0500

    update translations

--- locale/en_US/translations.ts
+++ locale/en_US/translations.ts
@@ -5,28 +5,107 @@
     <context>
         <name>default</name>
         <message>
-            <source>MenuOptionA</source>
-            <translation>Home</translation>
+            <source>Live Channels</source>
+            <translation>Live Channels</translation>
         </message>
         <message>
-            <source>MenuOptionB</source>
+            <source>Clips</source>
+            <translation>Clips</translation>
+        </message>
+        <message>
+            <source>Browse</source>
+            <translation>Browse</translation>
+        </message>
+        <message>
+            <source>Categories</source>
             <translation>Categories</translation>
         </message>
         <message>
-            <source>MenuOptionC</source>
+            <source>Following</source>
             <translation>Following</translation>
         </message>
         <message>
-            <source>MenuOptionD</source>
-            <translation>Vods</translation>
+            <source>Offline Channels</source>
+            <translation>Offline Channels</translation>
         </message>
         <message>
-            <source>LoginPageTopText</source>
+            <source>Login</source>
+            <translation>Login</translation>
+        </message>
+        <message>
+            <source>Live Stream</source>
+            <translation>Live Stream</translation>
+        </message>
+        <message>
+            <source>Recent Videos</source>
+            <translation>Recent Videos</translation>
+        </message>
+        <message>
+            <source>Search</source>
+            <translation>Search</translation>
+        </message>
+        <message>
+            <source>Go to https://www.twitch.tv/activate and type the following code:</source>
             <translation>Go to https://www.twitch.tv/activate and type the following code:</translation>
         </message>
         <message>
-            <source>LoginPageBottomText</source>
+            <source>Stitch should log you in soon after you authorize your account for use.</source>
             <translation>Stitch should log you in soon after you authorize your account for use.</translation>
+        </message>
+        <message>
+            <source>LIVE</source>
+            <translation>LIVE</translation>
+        </message>
+        <message>
+            <source>Chat On-By-Default</source>
+            <translation>Chat On-By-Default</translation>
+        </message>
+        <message>
+            <source>Determines default state of chat window when opening a stream</source>
+            <translation>Determines default state of chat window when opening a stream</translation>
+        </message>
+        <message>
+            <source>Video Quality</source>
+            <translation>Video Quality</translation>
+        </message>
+        <message>
+            <source>Video Framerate</source>
+            <translation>Video Framerate</translation>
+        </message>
+        <message>
+            <source>Home</source>
+            <translation>Home</translation>
+            <context>User's "home" page / fyp</context>
+        </message>
+        <message>
+            <source>Viewers</source>
+            <translation>Viewers</translation>
+            <context>displayed after the count of current viewership of a stream. i.e. this streamer has 200 viewers</context>
+        </message>
+        <message>
+            <source>Views</source>
+            <translation>Views</translation>
+            <context>The count of users that have watched a VOD/CLIP</context>
+        </message>
+        <message>
+            <source>Followers</source>
+            <translation>Followers</translation>
+            <context>Count of users that are currenlty 'following' a user on their Twitch account</context>
+        </message>
+        <message>
+            <source>Log Out</source>
+            <translation>Log Out</translation>
+            <context>Shown on the user's profile page to disconnect their account from the app</context>
+        </message>
+        <message>
+            <source>followedLiveUsers</source>
+            <translation>They're Live Right Now!</translation>
+            <context>Title shown above a list of streamers that the app user follows that are currently live streaming</context>
+        </message>
+        <message>
+            <source>followedOfflineUsers</source>
+            <translation>Offline Channels</translation>
+            <context>Shown above a list of streamers that the app user follows that are not currently live</context>
         </message>
     </context>
 </TS>
